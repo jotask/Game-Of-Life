bda9967df9e6e9bfa0ea308eabb2a941aff92dc3
Jose Vives <jota_sk@hotmail.com>
2015-08-29 12:56:24 +0200

    Created the GDX application with his configuration

--- Game of Life/src/uk/ac/aber/users/jov2/gameoflife/Application.java
+++ Game of Life/src/uk/ac/aber/users/jov2/gameoflife/Application.java
@@ -1,13 +1,57 @@
 package uk.ac.aber.users.jov2.gameoflife;
 
-public class Application {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Application implements ApplicationListener {
 	
-	public Application() {
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
 		
 	}
 
 	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		config.fullscreen = false;
+		config.resizable = false;
+		config.title = "Game of Life";
 		
+		new LwjglApplication(new Application(), config);
 	}
 
 }
